1041b6a56fa3b13d9cf6e17c544914d446e36301
BulliedDog <albertorovai1@gmail.com>
2021-08-15 19:48:14 +0200

    Add project files.

---
+++ TelegramBot/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TelegramBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
